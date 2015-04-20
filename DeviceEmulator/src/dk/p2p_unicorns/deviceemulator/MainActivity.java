c5860b6b1c5d27c1b593c666565de49ee5dfa92e
Casper <casperc91@gmail.com>
2015-04-20 10:23:26 +0200

    Added mainactivity

---
+++ DeviceEmulator/src/dk/p2p_unicorns/deviceemulator/MainActivity.java
@@ -0,0 +1,14 @@
+package dk.p2p_unicorns.deviceemulator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_layout);
+	}
+
+}
